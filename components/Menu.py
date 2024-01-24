285d30dd5de07f1548bdc3ae71739d4819976e00
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-01-24 17:55:22 -0300

    added verify title and message from error

--- components/Menu.py
+++ components/Menu.py
@@ -3,17 +3,17 @@ from components.Utils import clear_console
 
 def print_menu():
     menu = (
-        "1. Adicionar Tarefa",
-        "2. Ver tarefa",
-        "3. Atualizar Tarefa",
-        "4. Deletar Tarefa",
-        "5. Completar tarefa",
-        "6. Deletar tarefas completadas",
-        "7. Sair"
+        "Adicionar Tarefa",
+        "Ver tarefas",
+        "Atualizar Tarefa",
+        "Deletar Tarefa",
+        "Completar tarefa",
+        "Deletar tarefas completadas",
+        "Sair"
     )
 
     print("\nMenu Gerenciador de tarefas")
-    [print(item) for item in menu]
+    [print(f"{i + 1}. {item}") for i, item in enumerate(menu)]
 
 def Menu():
     tasks = Tasks()
@@ -24,14 +24,14 @@ def Menu():
 
         selected_item = input("Digite sua escolha: ")
 
-        if not selected_item.isdigit():
-            print('\nEntrada inválida!\n')
+        try:
+            selected_item = int(selected_item)
+        except ValueError:
+            print('\nEntrada inválida! Digite um número.\n')
             break
 
-        selected_item = int(selected_item)
-
         if selected_item not in range(1, 8):
-            print('\nEscolha inválida!!\n')
+            print('\nEscolha inválida!\n')
         elif selected_item == 7:
             break
         else:
@@ -39,22 +39,40 @@ def Menu():
 
 def handle_menu_choice(choice, tasks):
     clear_console()
+    tasks.show()
     match choice:
         case 1:
             title = input('\n Adicione o título da tarefa: ')
-            tasks.add(title)   
-        case 2:
-            tasks.show()
-            input("Pressione ENTER para continuar")
+            if len(title.strip()) < 3:
+                print("O título deve conter pelo menos 3 caracteres")
+                pass
+            else:
+                tasks.add(title)   
+                print(f'Tarefa "{title}" adicionada na lista!')
+
         case 3:
             task_id = int(input('\n Digite o ID da tarefa: '))
-            text = input('\n Digite o texto atualizado: ')
-            tasks.update(task_id, text)
+            text = input('\n Digite o título atualizado: ')
+            
+            if len(text.strip()) < 3:
+                print("O título deve conter pelo menos 3 caracteres")
+                pass
+            else:
+                tasks.update(task_id, text)
+                print(f'A tarefa foi renomeada para "{text}" com sucesso!')
+
         case 4:
             task_id = int(input('\n Digite o ID da tarefa: '))
             tasks.remove(task_id)
+            print(f'A tarefa com ID {task_id} foi removida com sucesso!')
+            
         case 5:
             task_id = int(input('\n Digite o ID da tarefa: '))
             tasks.mark_completed(task_id)
+            print(f'Tarefa com ID {task_id} concluída com sucesso!')
+
         case 6:
             tasks.removeCompleted()
+            print(f'Tarefas concluídas removidas da lista!')
+
+    input("Pressione ENTER para continuar")
